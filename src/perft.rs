7cf51a3f13d7acfc8b21ac57c1adf026c228a26b
Kirill Trofimov <kitrofimov39@gmail.com>
2025-06-19 14:10:22 +0500

    Ignore `perft` tests (for now)

--- src/perft.rs
+++ src/perft.rs
@@ -23,6 +23,7 @@ mod tests {
     use crate::position::Position;
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Initial_Position
     fn perft_initial_0_5() {
         let mut game = Game::default();
@@ -35,6 +36,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_2
     fn perft_kiwipete_1_5() {
         let mut game = Game::new(Position::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - "));
@@ -46,6 +48,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_3
     fn perft_position3_1_5() {
         let mut game = Game::new(Position::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 "));
@@ -57,6 +60,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_4
     fn perft_position4_1_5() {
         let mut game = Game::new(Position::from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"));
@@ -68,6 +72,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_5
     fn perft_position5_1_5() {
         let mut game = Game::new(Position::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"));
@@ -79,6 +84,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_6
     fn perft_position6_1_5() {
         let mut game = Game::new(Position::from_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"));
