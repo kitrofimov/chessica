eb336904e8eafef21acfe1898477cb5f7be7bd34
Kirill Trofimov <kitrofimov39@gmail.com>
2025-06-20 19:47:58 +0500

    Un-ignore `perft` tests; comment out the slow ones

--- src/perft.rs
+++ src/perft.rs
@@ -32,7 +32,6 @@ mod tests {
     use crate::position::Position;
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Initial_Position
     fn perft_initial_0_5() {
         let mut game = Game::default();
@@ -45,7 +44,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_2
     fn perft_kiwipete_1_5() {
         let mut game = Game::new(Position::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - "));
@@ -53,11 +51,10 @@ mod tests {
         assert_eq!(perft(&mut game, 2, 0), 2_039);
         assert_eq!(perft(&mut game, 3, 0), 97_862);
         assert_eq!(perft(&mut game, 4, 0), 4_085_603);
-        assert_eq!(perft(&mut game, 5, 0), 193_690_690);
+        // assert_eq!(perft(&mut game, 5, 0), 193_690_690);
     }
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_3
     fn perft_position3_1_5() {
         let mut game = Game::new(Position::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 "));
@@ -69,7 +66,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_4
     fn perft_position4_1_5() {
         let mut game = Game::new(Position::from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"));
@@ -77,11 +73,10 @@ mod tests {
         assert_eq!(perft(&mut game, 2, 0), 264);
         assert_eq!(perft(&mut game, 3, 0), 9_467);
         assert_eq!(perft(&mut game, 4, 0), 422_333);
-        assert_eq!(perft(&mut game, 5, 0), 15_833_292);
+        // assert_eq!(perft(&mut game, 5, 0), 15_833_292);
     }
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_5
     fn perft_position5_1_5() {
         let mut game = Game::new(Position::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"));
@@ -89,11 +84,10 @@ mod tests {
         assert_eq!(perft(&mut game, 2, 0), 1_486);
         assert_eq!(perft(&mut game, 3, 0), 62_379);
         assert_eq!(perft(&mut game, 4, 0), 2_103_487);
-        assert_eq!(perft(&mut game, 5, 0), 89_941_194);
+        // assert_eq!(perft(&mut game, 5, 0), 89_941_194);
     }
 
     #[test]
-    #[ignore]
     // https://www.chessprogramming.org/Perft_Results#Position_6
     fn perft_position6_1_5() {
         let mut game = Game::new(Position::from_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"));
@@ -101,6 +95,6 @@ mod tests {
         assert_eq!(perft(&mut game, 2, 0), 2_079);
         assert_eq!(perft(&mut game, 3, 0), 89_890);
         assert_eq!(perft(&mut game, 4, 0), 3_894_594);
-        assert_eq!(perft(&mut game, 5, 0), 164_075_551);
+        // assert_eq!(perft(&mut game, 5, 0), 164_075_551);
     }
 }
